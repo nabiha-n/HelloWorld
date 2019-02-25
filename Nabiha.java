d881d58c3d3fbce248374821eb937a02ca9bff96
naiha <nabiha.nbili123@gmail.com>
2019-02-25 23:46:08 +0100

    added HelloWorld program

---
+++ Nabiha.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ */
+ package hello;
+public class Nabiha  {
+    public static void main(String[]args){
+        for(int i=1;i<=12;i++){
+            System.out.println("Hello World..."+i);
+        }
+    }
+}
